83c36a0ab3e8f30907fb5cae50d797e17cc66909
yangwei <897900564@qq.com>
2021-07-05 17:23:20 +0800

    使用 spring-data-mongodb

--- mongo-example-parent/01-mongo-primary/src/main/java/com/yw/mongo/example/po/User.java
+++ mongo-example-parent/01-mongo-primary/src/main/java/com/yw/mongo/example/po/User.java
@@ -1,5 +1,6 @@
 package com.yw.mongo.example.po;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -8,6 +9,7 @@ import lombok.experimental.Accessors;
  */
 @Data
 @Accessors(chain = true)
+@AllArgsConstructor
 public class User {
     private String name;
     private int age;
